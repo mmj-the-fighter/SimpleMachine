1441f0d989d4e51ea5a2703540d2c1f58c7c0ef2
Manoj M J <mmjthefighter@gmail.com>
2025-10-25 20:34:04 +0530

    Update asm program

--- SimpleMachine/program.asm
+++ SimpleMachine/program.asm
@@ -1,15 +1,21 @@
 #Calculate the sum of numbers stored in 
 #consecutive memory locations starting from location 200
 #the number of elements is stored in location 199
+
+#intialization part
 MVI A 0		# initialize register A with 0
 LOAD C 199	# load count into register C from memory location 199
 MVI B 200	# initialize register B with 200
+
+#main logic
 LOOP:		# loop
 LDR D B		# load the value pointed by B to D
 ADD3 A A D	# A = A + D
 INC B		# ++B
 DCR C		# --C
 JNZ LOOP	# if C != 0 go to loop
+
+#output results
 STORE A 255	# store A to 255
 DISP A		# print A	
 HLT			# stop
